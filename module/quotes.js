7b37bef445fd2ee5d70210a5bae8343357983c31
Monika Cyprys <monko.projects@gmail.com>
2021-09-05 17:13:49 +0200

    add quotes

---
+++ module/quotes.js
@@ -0,0 +1,36 @@
+(function () {
+  function showQuote() {
+    const url = "http://quotes.stormconsultancy.co.uk/random.json";
+
+    async function getRandomQuote(url) {
+      const { quote, author } = await fetch(url)
+        .then((r) => r.json())
+        .then((r) => r);
+      return {
+        quote: quote,
+        author: author,
+      };
+    }
+    const randomQuote = getRandomQuote(url)
+      .then((result) => result)
+      .catch(() => console.log("sth wrong"));
+
+    async function getNewQuote() {
+      const { quote, author } = await randomQuote();
+      putNewQuote(quote, author);
+      return quote;
+    }
+    function putNewQuote(quote, author) {
+      const description = document.querySelector(".quote__description");
+      const authorName = document.querySelector(".quote__author");
+
+      description.textContent = `"${quote}"`;
+      authorName.textContent = `- ${author}`;
+    }
+    getNewQuote();
+  }
+
+  window.setInterval(function () {
+    showQuote();
+  }, 4000);
+})();
