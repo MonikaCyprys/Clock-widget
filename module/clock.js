a3306760ba212c7fc31eb43b42ceadb839657db0
Monika Cyprys <monko.projects@gmail.com>
2021-09-10 19:50:09 +0200

    add welcome note

--- module/clock.js
+++ module/clock.js
@@ -13,7 +13,6 @@
     return {
       city: city,
       country: country_code2,
-      time: current_time,
     };
   }
   const location = getLocationAndTime(url)
@@ -21,15 +20,15 @@
     .catch(() => console.log("sth wrong"));
 
   async function getNewLocationAndTime() {
-    const { city, country, time } = await location;
+    const { city, country } = await location;
+    const actualTime = showDataTime();
+    showBackround(city, actualTime);
     showDataLocation(city, country);
-    showDataTime(time);
-    
   }
 
   function showDataLocation(city, country) {
     const location = document.querySelector(".clock__location");
-    location.textContent = `${city}, ${country}`;
+    location.textContent = `In ${city}, ${country}`;
   }
   function showDataTime() {
     const clock = document.querySelector(".clock__time");
@@ -39,11 +38,52 @@
 
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
-
     const actualTime = h + ":" + m;
 
     clock.textContent = actualTime;
     setTimeout(showDataTime, 1000);
+    return actualTime;
+  }
+
+  async function getSunsetAndSunrise(url, city) {
+    const astroUrl = url.replace("ipgeo?", "astronomy?");
+    const { sunrise, sunset } = await fetch(astroUrl + "location=" + city)
+      .then((r) => r.json())
+      .then((r) => r);
+    return {
+      sunrise: sunrise,
+      sunset: sunset,
+    };
+  }
+
+  async function showBackround(city, actualTime) {
+    const bg = document.querySelector(".welcome-side");
+    const clock = document.querySelector(".clock__welcome");
+    const icon = document.querySelector(".fa-sun");
+
+    const dayTime = getSunsetAndSunrise(url, city)
+      .then((result) => result)
+      .catch(() => console.log("sth wrong"));
+
+    let { sunrise, sunset } = await dayTime;
+
+    sunset = sunset.split(":").join("");
+    sunrise = sunrise.split(":").join("");
+    actualTime = actualTime.split(":").join("");
+
+    if (actualTime > sunrise && actualTime < sunset) {
+      const toLight = bg.style.backgroundImage.replace("dark", "light");
+      bg.style.backgroundImage = toLight;
+      clock.textContent = " Good morning,";
+      icon.classList.remove("fa-moon");
+      icon.classList.add("fa-sun");
+    } else {
+      const toDark = bg.style.backgroundImage.replace("light", "dark");
+      bg.style.backgroundImage = toDark;
+      clock.textContent = " Good night,";
+      icon.classList.remove("fa-sun");
+      icon.classList.add("fa-moon");
+    }
   }
 
   getNewLocationAndTime();
